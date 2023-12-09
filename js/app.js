9b89c5c10b21c9749aa90c32024dca1a81caefbc
saradonin <m.jemieljanczuk@gmail.com>
2023-12-09 19:09:45 +0100

    error handling added, problem with polish letters solved

--- js/app.js
+++ js/app.js
@@ -5,84 +5,96 @@ const apiKey = "53f070eb45aa431085b223028230712"
 Returns weather JSON based on city name
  */
 async function getData(city) {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5
-`)
-    return await response.json()
+    try {
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5`);
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching weather data:", error.message);
+        throw error;
+    }
 }
+
 
 /*
 Created weather window for a city
  */
 async function createElements(city = "auto:ip") {
-    document.body.classList.add("loading")
-    const weatherData = await getData(city)
-    console.log(weatherData)
+    try {
+        document.body.classList.add("loading")
+        const weatherData = await getData(city)
+        console.log(weatherData)
 
-    // Clone and add module
-    const $originalWeatherModule = document.querySelector(".module__weather")
-    const newWeatherModule = $originalWeatherModule.cloneNode(true)
-    newWeatherModule.removeAttribute("hidden")
+        // Clone and add module
+        const $originalWeatherModule = document.querySelector(".module__weather")
+        const newWeatherModule = $originalWeatherModule.cloneNode(true)
+        newWeatherModule.removeAttribute("hidden")
 
-    const app = document.querySelector("#app")
-    app.appendChild(newWeatherModule)
+        const app = document.querySelector("#app")
+        app.appendChild(newWeatherModule)
 
-    // Set data items
-    const cityName = weatherData.location.name
-    const $cityName = newWeatherModule.querySelector(".city__name")
-    $cityName.textContent = cityName
+        // Set data items
+        const cityName = weatherData.location.name
+        const $cityName = newWeatherModule.querySelector(".city__name")
+        $cityName.textContent = cityName
 
-    const currentTemperature = weatherData.current.temp_c
-    const $currentTemperature = newWeatherModule.querySelector(".temperature__value")
-    $currentTemperature.textContent = currentTemperature
+        const currentTemperature = weatherData.current.temp_c
+        const $currentTemperature = newWeatherModule.querySelector(".temperature__value")
+        $currentTemperature.textContent = currentTemperature
 
-    const currentPressure = weatherData.current.pressure_mb
-    const $currentPressure = newWeatherModule.querySelector(".pressure__value")
-    $currentPressure.textContent = `${currentPressure} hPa`
+        const currentPressure = weatherData.current.pressure_mb
+        const $currentPressure = newWeatherModule.querySelector(".pressure__value")
+        $currentPressure.textContent = `${currentPressure} hPa`
 
-    const currentHumidity = weatherData.current.humidity
-    const $currentHumidity = newWeatherModule.querySelector(".humidity__value")
-    $currentHumidity.textContent = `${currentHumidity}%`
+        const currentHumidity = weatherData.current.humidity
+        const $currentHumidity = newWeatherModule.querySelector(".humidity__value")
+        $currentHumidity.textContent = `${currentHumidity}%`
 
-    const currentWindSpeed = weatherData.current.wind_kph
-    const $currentWindSpeed = newWeatherModule.querySelector(".wind-speed__value")
-    $currentWindSpeed.textContent = `${(currentWindSpeed / 3.6).toFixed(1)} m/s`
+        const currentWindSpeed = weatherData.current.wind_kph
+        const $currentWindSpeed = newWeatherModule.querySelector(".wind-speed__value")
+        $currentWindSpeed.textContent = `${(currentWindSpeed / 3.6).toFixed(1)} m/s`
 
-    const currentCondition = weatherData.current.condition.text
-    const $currentConditionIcon = newWeatherModule.querySelector(".weather__icon img")
-    $currentConditionIcon.src = setIcon(currentCondition)
+        const currentCondition = weatherData.current.condition.text
+        const $currentConditionIcon = newWeatherModule.querySelector(".weather__icon img")
+        $currentConditionIcon.src = setIcon(currentCondition)
 
 
-    // Forecast section
-    const forecast = weatherData.forecast.forecastday
-    const $forecastList = newWeatherModule.querySelector(".weather__forecast")
-    forecast.forEach((day) => {
-        const dayDate = new Date(day.date); // Convert the date string to a Date object
-        const dayName = getDayName(dayDate.getDay())
-        const dayCondition = day.day.condition.text
-        const dayTemp = day.day.maxtemp_c
+        // Forecast section
+        const forecast = weatherData.forecast.forecastday
+        const $forecastList = newWeatherModule.querySelector(".weather__forecast")
+        forecast.forEach((day) => {
+            const dayDate = new Date(day.date); // Convert the date string to a Date object
+            const dayName = getDayName(dayDate.getDay())
+            const dayCondition = day.day.condition.text
+            const dayTemp = day.day.maxtemp_c
 
-        const li = document.createElement("li")
-        const dayNameSpan = document.createElement("span")
-        dayNameSpan.classList.add("day")
-        dayNameSpan.textContent = dayName
-        const img = document.createElement("img")
-        img.src = setIcon(dayCondition)
+            const li = document.createElement("li")
+            const dayNameSpan = document.createElement("span")
+            dayNameSpan.classList.add("day")
+            dayNameSpan.textContent = dayName
+            const img = document.createElement("img")
+            img.src = setIcon(dayCondition)
 
-        const dayTempSpan = document.createElement("span")
-        dayTempSpan.classList.add("temperature")
-        const degreeSymbol = document.createTextNode("\u00B0C");  // creates &deg;C symbol
-        const dayTempValueSpan = document.createElement("span")
-        dayTempValueSpan.classList.add("temperature__value")
-        dayTempValueSpan.textContent = dayTemp
-        dayTempSpan.appendChild(dayTempValueSpan)
-        dayTempSpan.appendChild(degreeSymbol)
+            const dayTempSpan = document.createElement("span")
+            dayTempSpan.classList.add("temperature")
+            const degreeSymbol = document.createTextNode("\u00B0C");  // creates &deg;C symbol
+            const dayTempValueSpan = document.createElement("span")
+            dayTempValueSpan.classList.add("temperature__value")
+            dayTempValueSpan.textContent = dayTemp
+            dayTempSpan.appendChild(dayTempValueSpan)
+            dayTempSpan.appendChild(degreeSymbol)
 
-        li.appendChild(dayNameSpan)
-        li.appendChild(img)
-        li.appendChild(dayTempSpan)
-        $forecastList.appendChild(li)
-    });
-    updateModules()
+            li.appendChild(dayNameSpan)
+            li.appendChild(img)
+            li.appendChild(dayTempSpan)
+            $forecastList.appendChild(li)
+        });
+        updateModules()
+    } catch (error) {
+        console.error("Error creating elements:", error.message);
+    }
     document.body.classList.remove("loading")
 }
 
@@ -101,9 +113,6 @@ function setIcon(element) {
     const dirPath = 'assets/icons/'
     let icon = ""
     switch (true) {
-        case ["Rain", "rain"].some(word => element.includes(word)):
-            icon = "rain.svg"
-            break;
         case ["Rain", "rain"].some(word => element.includes(word)):
             icon = "rain.svg"
             break;
@@ -147,9 +156,20 @@ function updateModules() {
 }
 
 /*
+Replace Polish letters with English letters for search purpose
+ */
+function replacePolishLetters(text) {
+    const replacements = {
+        'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n', 'ó': 'o', 'ś': 's', 'ź': 'z', 'ż': 'z',
+        'Ą': 'A', 'Ć': 'C', 'Ę': 'E', 'Ł': 'L', 'Ń': 'N', 'Ó': 'O', 'Ś': 'S', 'Ź': 'Z', 'Ż': 'Z'
+    };
+    return text.replace(/[ąćęłńóśźż]/gi, (letter) => replacements[letter] || letter);
+}
+
+/*
 Adds new city
  */
-function addCity () {
+function addCity() {
     const $addCityButton = document.querySelector("#add-city")
     const $addCityForm = document.querySelector(".module__form")
 
@@ -162,16 +182,18 @@ function addCity () {
 
     $searchButton.addEventListener("click", (e) => {
         e.preventDefault()
-        createElements($searchInput.value.trim())
+        const searchCity = replacePolishLetters($searchInput.value.trim())
+        console.log(searchCity)
+        createElements(searchCity)
         $searchInput.value = ""
         $addCityForm.setAttribute("hidden", "")
     })
-
-
 }
 
-
-function main () {
+/*
+Main
+ */
+function main() {
     createElements()
     addCity()
 }
